5e6852d32eed1c0416069e88178b49b49b5ea428
LO <46852948+losterbr@users.noreply.github.com>
2026-08-01 22:00:30 +0100

    Make plotting executable non-interactive by default

--- apps/plotting.cpp
+++ apps/plotting.cpp
@@ -1,3 +1,16 @@
 #include "plotting_app.hpp"
 
-int main() { return run_plotting_app(); }
+#include <string_view>
+
+int main(int argc, char **argv)
+{
+    PlotOptions options;
+    options.invoke_gnuplot = false;
+
+    if (argc > 1 && std::string_view(argv[1]) == "--gnuplot")
+    {
+        options.invoke_gnuplot = true;
+    }
+
+    return run_plotting_app(options);
+}
